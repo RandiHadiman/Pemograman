6238597924485bfdb2a69646afc2d2f8cc7135ff
RandiHadiman <randihadiman49@gmail.com>
2022-11-02 18:38:25 +0700

    navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'secondpage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  GlobalKey<ScaffoldState> _scafoldkey = new GlobalKey<ScaffoldState>();
-
+class MyApp extends StatelessWidget {
+  // const SecondPage({ key? key }) : super(Key: key);
   @override
   Widget build(BuildContext context) {
-    showToast() {
-      setState(() {
-        Fluttertoast.showToast(
-            msg: 'This is notification from toast',
-            toastLength: Toast.LENGTH_LONG,
-            gravity: ToastGravity.CENTER);
-      });
-    }
-
-    showSnackbar() {
-      tState(() {
-        final snackBar = SnackBar(
-            content: Text("This is Notification from Snackbar"),
-            duration: Duration(seconds: 5),
-            action: SnackBarAction(label: 'Undo', onPressed: () {}));
-      });
-    }
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Latihan Notification"),
+          title: Text("Navigation Route"),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              MaterialButton(
-                onPressed: () {
-                  showToast();
-                },
-                child: Text("Show Toast"),
-                onPressed: () {
-                  showSnackbar();
-                },
-                child: Text("Show Snackbar"),
-              )
-            ],
-          ),
-        ),
+        body: Builder(
+            builder: ((context) => Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      MaterialButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: ((context) => secondpage())));
+                        },
+                        color: Colors.blue,
+                        child: Text(
+                          "Pergi Ke Halaman 2",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      )
+                    ],
+                  ),
+                ))),
+      ),
+    );
+  }
+}
+
+class SnackBarPage extends StatelessWidget {
+  const SnackBarPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          final snackBar = SnackBar(
+            content: const Text('Yay! A SnackBar!'),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () {
+                // Some code to undo the change.
+              },
+            ),
+          );
+
+          // Find the ScaffoldMessenger in the widget tree
+          // and use it to show a SnackBar.
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        },
+        child: const Text('Show SnackBar'),
       ),
     );
   }
